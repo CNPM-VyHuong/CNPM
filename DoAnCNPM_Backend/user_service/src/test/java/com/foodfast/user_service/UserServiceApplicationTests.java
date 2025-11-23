6321b3fe3a5142253ea0a2c7c5cab8e026c5c000
NGUYEN-HUONG-web <huonghanna10052004@gmail.com>
2025-11-24 00:57:35 +0700

    Demo: Add failing test case to demonstrate test failure tracking in Grafana

--- DoAnCNPM_Backend/user_service/src/test/java/com/foodfast/user_service/UserServiceApplicationTests.java
+++ DoAnCNPM_Backend/user_service/src/test/java/com/foodfast/user_service/UserServiceApplicationTests.java
@@ -2,12 +2,18 @@ package com.foodfast.user_service;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 class UserServiceApplicationTests {
 
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	void testFailure() {
+		assertEquals(1, 2, "This test intentionally fails to demonstrate failure in Grafana");
 	}
 
 }
